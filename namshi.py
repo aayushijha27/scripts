8e5b5b0f4559522b4cb7dadb62e5d0596a6fd7e3
aayushijha27 <40524775+aayushijha27@users.noreply.github.com>
2020-05-03 11:28:59 +0530

    Add files via upload

--- namshi.py
+++ namshi.py
@@ -1,53 +1,75 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from bs4 import BeautifulSoup
 import csv
-import pandas as pd
 
-class NamshiSpider(scrapy.Spider):
-	name = 'namshi'
-	custom_settings = {'HTTPERROR_ALLOW_ALL': True}
-	handle_httpstatus_list = [301,500, 502, 503, 504, 400, 403, 408]
+class NamshiSpider1(scrapy.Spider):
+	name = 'namshi1'
 	start_urls = ['https://www.namshi.com/']
  
-	def __init__(self,text=None,*args,**kwargs):
+	def __init__(self,category=None,subcategory=None,*args,**kwargs):
 		self.headers = {
 			'Accept':'application/json, text/plain, */*',
 			'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
 			'Sec-Fetch-Mode':'cors',
 		}
-	def parse(self, response):
-		with open('test.csv','r') as f:
-			reader = csv.reader(f)
-			next(reader)
-			for row in reader:
-				product_url = row[13]
-				# url = 'https://en-sa.namshi.com/buy-nike-nsw-shine-cropped-t-shirt-w589034a.html'
-				yield scrapy.Request(url=product_url,headers=self.headers,callback=self.parse1)
+		self.category = category
+		self.subcategory = 'new-arrivals'
 	
-	def parse1(self, response):
-		try:
-			product_price = response.xpath("//span[@class='pre-reduction']/text()").extract()
-			if product_price != " ": 
-				brand_name = ''.join(response.xpath("//h2/a/text()").extract())
-				product_url = response.url
-				product_name = ''.join(response.xpath("//h1[@class='product__name']/text()").extract())
-				price = response.xpath("//div[@class='product__details']/p/span/text()").extract()[0]
-				reduction_price = response.xpath("//span[@class='reduction']/text()").extract()
-				product_usps = ''.join(response.xpath("//section[@class='product-usps']/div/text()").extract())
-				delivery = ''.join(response.xpath("//p[@class='product__deliveryestimation']/text()").extract()).strip()
-				rewards = ''.join(response.xpath("//div[@class='skywards_reward_miles']/text()").extract())
-				product_description = ''.join(response.xpath("//div[@class='info_shortdescription info_content']/ul/li/text()").extract())
-				info_care = ''.join(response.xpath("//div[@class='info_content']/table/tr/th/text()").extract())
-				offer = ''.join(response.xpath("//div[@class='catalog-tag']/text()").extract())
-				product_size = response.xpath("//ul[@id='product_size']/li/label/span/text()").extract()
-				img_url = ''.join(response.xpath('//*[@id="product_carousel"]/div[2]/img/@src').extract())
-		# import pdb; pdb.set_trace()
-		except:
-			pass
-		item = {'Brand name' : brand_name,
+	def start_requests(self):
+		# if self.category == 'kids':
+		# 	url = 'https://en-sa.namshi.com/kids/' + str(self.subcategory)
+		# 	yield scrapy.Request(url=url,headers=self.headers,callback=self.parse)
+		# elif self.category == 'women':
+		# 	url = 'https://en-sa.namshi.com/women/' + str(self.subcategory)
+		# 	yield scrapy.Request(url=url,headers=self.headers,callback=self.parse)
+		# elif self.category == 'men':
+		# 	url = 'https://en-sa.namshi.com/men/' + str(self.subcategory)
+		# 	yield scrapy.Request(url=url,headers=self.headers,callback=self.parse)
+
+		url = 'https://en-global.namshi.com/'
+		yield scrapy.Request(url=url,headers=self.headers,callback=self.parse)
+	
+	def parse(self,response):
+		pages = int(''.join(response.xpath('//ul[@id="pagination"]/p/text()').extract()).strip().split('of')[1].strip())
+		for i in range(1,pages+1):
+			url = response.url + 'page-' + str(i)
+			yield scrapy.Request(url=url,headers=self.headers,callback=self.parse1)
+	
+	def parse1(self,response):
+		leng = len(response.xpath('//*[@id="catalog_listings"]/li').extract())
+		for i in range(1,leng+1):
+			product_link = 'https://en-sa.namshi.com' + ''.join(response.xpath('//*[@id="catalog_listings"]/li['+str(i)+']/a/@href').extract())
+			yield scrapy.Request(url=product_link,headers=self.headers,callback=self.parse2)
+	
+	def parse2(self,response):
+		brand_name = response.xpath("//h2/a/text()").extract()
+		product_url = response.url
+		product_name = ''.join(response.xpath("//h1[@class='product__name']/text()").extract())
+		product_price = ''.join(response.xpath("//span[@class='pre-reduction']/text()").extract())
+		price = ''.join(response.xpath("//div[@class='product__details']/p/span/text()").extract()[0])
+		reduction_price = ''.join(response.xpath("//span[@class='reduction']/text()").extract())
+		product_usps = ''.join(response.xpath("//section[@class='product-usps']/div/text()").extract())
+		delivery = ''.join(response.xpath("//p[@class='product__deliveryestimation']/text()").extract()).strip()
+		rewards = ''.join(response.xpath("//div[@class='skywards_reward_miles']/text()").extract())
+		product_description = ''.join(response.xpath("//div[@class='info_shortdescription info_content']/ul/li/text()").extract())
+		offer = ''.join(response.xpath("//div[@class='catalog-tag']/text()").extract())
+		product_size = ''.join(response.xpath("//ul[@id='product_size']/li/label/span/text()").extract())
+		img_url = ''.join(response.xpath('//*[@id="product_carousel"]/div[2]/img/@src').extract())
+
+		soup = BeautifulSoup(response.text,'lxml')
+
+		table = soup.find('table',class_='product_attributes')
+		info_care = {}
+		for tr in table.findAll('tr'):
+			key = tr.findAll('th')[0].text.strip()
+			value = tr.findAll('th')[1].text.strip()
+			info_care[key] = value
+  		
+		item = {
+				'Brand name' : brand_name,
 				'Product name' : product_name,
 				'Product price' : product_price,
-				'Price' : price,
 				'Reduction price' : reduction_price,
 				'Product usps' : product_usps,
 				'Delivery' : delivery,
@@ -61,6 +83,4 @@ class NamshiSpider(scrapy.Spider):
 				'Image url' : img_url
 				
 				}  
-		yield item
-
-		
+		yield item     
